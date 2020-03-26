4f3e322851af96220e164d9ae9930855b449b3ee
ludrianowashington <ludriano.washi@gmail.com>
2020-03-25 23:53:03 -0300

    Frontend da aplicação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -42,8 +42,6 @@ module.exports = {
     const { id } = request.params;
     const ong_id = request.headers.authorization;
 
-    console.log(id, ong_id);
-
     const incident = await connection("incidents")
       .where("id", id)
       .select("ong_id")
